850958e8b3939b26b750b36ac81ec980ca714561
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-19 21:14:58 +0800

    feat: move Copy to Clipboard button

--- src/components/ui/CodeBlock.tsx
+++ src/components/ui/CodeBlock.tsx
@@ -15,26 +15,6 @@ export default function CodeBlock({ content, className }: PropTypes) {
 
   return (
     <>
-      <button
-        onClick={() => {
-          copyTextToClipboard(content);
-        }}
-        className="md:hidden flex text-lg items-center justify-center gap-2 w-full font-semibold py-[0.75rem] p-[0.5rem] mb-2 rounded-md hover:bg-zinc-200 transition-colors"
-        aria-label="Copy to Clipboard"
-        type="button"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          fill="currentColor"
-        >
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
-        </svg>
-        Copy to Clipboard
-      </button>
       <pre className={`relative ${className}`}>
         <code className="language-javascript">{content}</code>
         <button
@@ -57,6 +37,26 @@ export default function CodeBlock({ content, className }: PropTypes) {
           </svg>
         </button>
       </pre>
+      <button
+        onClick={() => {
+          copyTextToClipboard(content);
+        }}
+        className="md:hidden flex text-lg items-center justify-center gap-2 w-full font-semibold py-[0.75rem] p-[0.5rem] mt-4 rounded-md hover:bg-zinc-200 transition-colors"
+        aria-label="Copy to Clipboard"
+        type="button"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 0 24 24"
+          width="24px"
+          fill="currentColor"
+        >
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+        </svg>
+        Copy to Clipboard
+      </button>
     </>
   );
 }
