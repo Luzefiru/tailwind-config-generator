7e49a5a25dc8c781dad8dc7af5d4d15edbedd250
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-20 13:40:01 +0800

    feat: make ColorCard more accessible

--- src/components/ColorCard.tsx
+++ src/components/ColorCard.tsx
@@ -6,30 +6,30 @@ interface PropTypes {
 }
 
 export default function ColorCard({ name, value }: PropTypes) {
-  const [isShowingValue, setIsShowingValue] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
   const ColorCardText = useRef(null);
 
   return (
-    <div className="bg-white w-full rounded-md shadow-sm cursor-pointer relative">
+    <div
+      tabIndex={0}
+      className="bg-white w-full rounded-md shadow-sm cursor-pointer relative"
+      onFocus={() => {
+        setIsFocused(!isFocused);
+      }}
+      onBlur={() => {
+        setIsFocused(!isFocused);
+      }}
+    >
       <div
+        aria-label={isFocused ? value : name}
         style={{ backgroundColor: value }}
         className="h-16 w-full rounded"
-        onMouseEnter={(e) => {
-          if (e.target !== ColorCardText.current) {
-            setIsShowingValue(!isShowingValue);
-          }
-        }}
-        onMouseLeave={(e) => {
-          if (e.target !== ColorCardText.current) {
-            setIsShowingValue(!isShowingValue);
-          }
-        }}
       ></div>
       <div
         ref={ColorCardText}
         className="text-[2.5vw] -bottom-[1.115rem] md:text-[0.75rem] md:-bottom-[1.5rem] font-medium text-neutral-700 min-w-full whitespace-nowrap absolute  text-center cursor-text"
       >
-        <code>{isShowingValue ? value : name}</code>
+        <code>{isFocused ? value : name}</code>
       </div>
     </div>
   );
