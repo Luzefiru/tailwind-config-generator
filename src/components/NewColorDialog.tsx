e39889eaf7b896c2ede4aa7509fb4d2e2c34e678
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-21 20:14:10 +0800

    feat: remove ColorDialog file state when cancelled

--- src/components/NewColorDialog.tsx
+++ src/components/NewColorDialog.tsx
@@ -16,45 +16,55 @@ export default function NewColorDialog({ className, handleClose }: PropTypes) {
     }
   };
 
+  const handleCancel = () => {
+    setImage(undefined);
+    handleClose();
+  };
+
   return (
     <>
       <div
         onClick={handleClose}
         className={`cover bg-zinc-700 opacity-40 ${className}`}
       />
-      <div className={`dialog fade-in ${className}`}>
-        <div className="flex flex-col p-4 rounded-lg bg-neutral-100 relative w-[80vw] -ml-[40vw] h-[80vh] -mt-[40vh] lg:w-[40vw] lg:-ml-[20vw]">
-          <div className="w-full flex justify-end">
-            <svg
-              onClick={handleClose}
-              className="cursor-pointer hover:text-red-500"
-              xmlns="http://www.w3.org/2000/svg"
-              height="48"
-              viewBox="0 96 960 960"
-              width="48"
-            >
-              <path
-                fill="currentColor"
-                d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"
-              />
-            </svg>
-          </div>
-          <div className="overflow-hidden flex-grow m-4 mb-0 bg-white shadow-sm rounded-lg flex items-center justify-center">
+      <section className={`dialog fade-in ${className}`}>
+        <div className="flex flex-col p-6 rounded-lg bg-neutral-100 relative w-[80vw] -ml-[40vw] h-[80vh] -mt-[40vh] lg:w-[40vw] lg:-ml-[20vw]">
+          <div className="overflow-hidden flex-grow mb-4 bg-white shadow-sm rounded-lg flex items-center justify-center">
             <img
               className="rounded object-contain p-2"
               alt="Your Upload"
               src={image ? image : ImageIcon}
             />
           </div>
-          <div className="m-4 p-4 bg-white shadow-sm rounded-lg flex items-center justify-between">
+          <div className="p-4 bg-white shadow-sm rounded-lg flex items-center justify-between">
             <div>
               <h2 className="mb-2 font-semibold">Upload an Image</h2>
-              <input className="text-sm" type="file" onChange={onImageChange} />
+              <input
+                aria-label="Upload a Custom Image"
+                className="text-sm"
+                type="file"
+                onChange={onImageChange}
+              />
             </div>
-            <input className="h-14 w-14 rounded-full" type="color" />
+            <input
+              aria-label="Choose a Color"
+              className="h-14 w-14 rounded-full"
+              type="color"
+            />
+          </div>
+          <div aria-label="Buttons" className="flex gap-4 pt-4 justify-end">
+            <button
+              onClick={handleCancel}
+              className="shadow-sm border-2 px-5 py-[0.4rem] rounded-md font-medium transition-colors border-red-600 text-red-600 hover:bg-red-700 hover:border-red-700 hover:text-white"
+            >
+              Cancel
+            </button>
+            <button className="shadow-sm border-2 px-5 py-[0.4rem] rounded-md font-medium transition-colors border-emerald-500 text-white bg-emerald-500 hover:bg-emerald-700 hover:border-emerald-700">
+              Add Color
+            </button>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
